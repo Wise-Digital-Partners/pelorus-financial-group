3a5681b1862f924e14dee88b61d596664743bb18
Hamlet <55106549+hamletavetisian@users.noreply.github.com>
2021-09-23 21:00:36 -0700

    Update bodyPortableText.js

--- studio/schemas/objects/bodyPortableText.js
+++ studio/schemas/objects/bodyPortableText.js
@@ -29,6 +29,7 @@ export default {
         decorators: [
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" },
+          { title: 'Code', value: 'code' },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
@@ -46,7 +47,6 @@ export default {
           },
         ],
       },
-      of: [{ type: "authorReference" }],
     },
     // You can add additional types here. Note that you can't use
     // primitive types such as 'string' and 'number' in the same array
